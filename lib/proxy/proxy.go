b225b53606e012ac21cea5099ff2ec790d49b70e
Ryan Hawkins <ryanlarryhawkins@gmail.com>
2022-02-20 00:37:27 -0700

    Initial stab at enabling TLS
    This is a first "stab" at getting TLS working for falcon. I do have
    tests written and I think it'll work, but I'd like to make TLS a little
    more like an ON switch, so you don't have to do any configuration on
    your container to get TLS on. That's in addition to the other automatic
    changes I'd like to make.

--- lib/proxy/proxy.go
+++ lib/proxy/proxy.go
@@ -1,15 +1,31 @@
 package proxy
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Hawkbawk/falcon/lib/docker"
+	"github.com/Hawkbawk/falcon/lib/shell"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/go-connections/nat"
+	"gopkg.in/yaml.v2"
 )
 
 const ProxyImageName = "hawkbawk/falcon-proxy"
 const ProxyContainerName = "falcon-proxy"
+const ProxyBaseDir = "/usr/src/app"
+const defaultConfig = `
+# This is where falcon will add any info about any certificates that it creates for you.
+# Alternatively, you can put any info about your own certificates here.
+# See https://doc.traefik.io/traefik/https/tls/#user-defined for more info.
+tls:
+  certificates: {}
+`
 
-var containerConfig container.Config = container.Config{
+var certificatesDir = fmt.Sprintf("%v/.falcon/certs", os.Getenv("HOME"))
+var dynamicConfigPath = fmt.Sprintf("%v/.falcon/dynamic.yml", os.Getenv("HOME"))
+
+var containerConfig *container.Config = &container.Config{
 	Image: ProxyImageName,
 	ExposedPorts: nat.PortSet{
 		"80": struct{}{},
@@ -21,9 +37,10 @@ var containerConfig container.Config = container.Config{
 	},
 }
 
-var hostConfig container.HostConfig = container.HostConfig{
+var hostConfig *container.HostConfig = &container.HostConfig{
 	Binds: []string{
 		"/var/run/docker.sock:/var/run/docker.sock:ro",
+		"~/.falcon:/usr/src/app",
 	},
 	PortBindings: nat.PortMap{
 		"80": []nat.PortBinding{
@@ -35,11 +52,120 @@ var hostConfig container.HostConfig = container.HostConfig{
 	},
 }
 
-// Start starts up the falcon-proxy so that it can start forwarding requests.
-func Start(client docker.DockerClient) error {
-	return client.StartContainer(ProxyImageName, &hostConfig, &containerConfig, ProxyContainerName)
+type TlsFilesConfig struct {
+	CertFile string `yaml:"certFile,omitempty"`
+	KeyFile  string `yaml:"keyFile,omitempty"`
 }
 
+type DynamicConfig struct {
+	Tls struct {
+		Certificates []TlsFilesConfig `yaml:"certificates,omitempty"`
+	} `yaml:"tls,omitempty"`
+}
+
+// Start starts up the falcon-proxy so that it can start forwarding requests.
+func Start(client docker.DockerClient) error {
+	return client.StartContainer(ProxyImageName, hostConfig, containerConfig, ProxyContainerName)
+}
+
+// Stop stops the falcon-proxy container.
 func Stop(client docker.DockerClient) error {
 	return client.StopAndRemoveContainer(ProxyContainerName)
+}
+
+// EnableTlsForHost creates the certificate files necessary for the specified
+// hostname in the falcon certs directory and adds them to the Traefik dynamic
+// config that gets mounted inside the falcon-proxy container.
+func EnableTlsForHost(hostname string) error {
+	if err := ensureDynamicConfig(); err != nil {
+		return err
+	}
+
+	if err := createTlsFiles(hostname, shell.RunCommand); err != nil {
+		return err
+	}
+
+	config, err := os.ReadFile(dynamicConfigPath)
+
+	if err != nil {
+		return err
+	}
+
+	newConfig, err := addFilesToConfig(hostname, config)
+
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(dynamicConfigPath, []byte(newConfig), 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// createTlsFiles creates the key and cert file for the specified hostname
+// in the falcon directory.
+func createTlsFiles(hostname string, cmdRunner func(string) error) error {
+
+	// In an ideal world, we'd just be able to tell mkcert where
+	// we want the certificates, but due to a bug in mkcerts arg parsing code,
+	// we can't do that. That would be much easier to test, but this works for
+	// now.
+	if err := os.Chdir(certificatesDir); err != nil {
+		return err
+	}
+
+	if err := cmdRunner(fmt.Sprintf("mkcert %v", hostname)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// addFilesToConfig adds the key and cert files for the specified hostname
+// to the Traefik dynamic config.
+func addFilesToConfig(hostname string, data []byte) ([]byte, error) {
+	config := DynamicConfig{}
+
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	certFilePath := fmt.Sprintf("%v/certs/%v", ProxyBaseDir, createCertFileName(hostname))
+	keyFilePath := fmt.Sprintf("%v/certs/%v", ProxyBaseDir, createKeyFileName(hostname))
+
+	config.Tls.Certificates = append(config.Tls.Certificates, TlsFilesConfig{CertFile: certFilePath, KeyFile: keyFilePath})
+
+	return yaml.Marshal(&config)
+}
+
+// createCertFileName returns the filename for a certificate for the specified
+// hostname.
+func createCertFileName(hostname string) string {
+	return fmt.Sprintf("%v.pem", hostname)
+}
+
+// createKeyFileName returns the filename for a key file for the specified
+// hostname.
+func createKeyFileName(hostname string) string {
+	return fmt.Sprintf("%v-key.pem", hostname)
+}
+
+// ensureDynamicConfig ensures both that the certificates directory exists,
+// and that the Traefik dynamic config exists as well.
+func ensureDynamicConfig() error {
+	if err := os.MkdirAll(certificatesDir, 0755); err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(dynamicConfigPath); os.IsNotExist(err) {
+		if err := os.WriteFile(dynamicConfigPath, []byte(defaultConfig), 0755); err != nil {
+			return err
+		} else {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ lib/proxy/proxy_test.go
@@ -0,0 +1,147 @@
+package proxy
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Hawkbawk/falcon/mocks/mock_docker"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v2"
+)
+
+var _ = Describe("Proxy", func() {
+	var (
+		ctrl       *gomock.Controller
+		mockClient *mock_docker.MockDockerClient
+		hostname   = "example.com"
+	)
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+		mockClient = mock_docker.NewMockDockerClient(ctrl)
+	})
+
+	Describe("Start", func() {
+		It("tries to start the proxy container and returns no errors", func() {
+			mockClient.EXPECT().StartContainer(ProxyImageName, hostConfig, containerConfig, ProxyContainerName).Return(nil)
+
+			Expect(Start(mockClient)).To(Succeed())
+		})
+
+		It("returns an error if the container can't be started", func() {
+			err := fmt.Errorf("problems!")
+			mockClient.EXPECT().StartContainer(ProxyImageName, hostConfig, containerConfig, ProxyContainerName).Return(err)
+
+			Expect(Start(mockClient)).To(Equal(err))
+		})
+	})
+
+	Describe("Stop", func() {
+		It("tries to stop the proxy container and returns no errors", func() {
+			mockClient.EXPECT().StopAndRemoveContainer(ProxyContainerName).Return(nil)
+
+			Expect(Stop(mockClient)).To(Succeed())
+		})
+
+		It("returns an error if the container can't be stopped", func() {
+			err := fmt.Errorf("problems!")
+			mockClient.EXPECT().StopAndRemoveContainer(ProxyContainerName).Return(err)
+
+			Expect(Stop(mockClient)).To(Equal(err))
+		})
+	})
+
+	Describe("createTlsFiles", func() {
+		var (
+			argList   []string
+			err       error
+			cmdRunner = func(cmd string) error {
+				argList = append(argList, cmd)
+				return err
+			}
+		)
+
+		BeforeEach(func() {
+			argList = make([]string, 0)
+			err = nil
+		})
+
+		It("tries to call mkcert to make the certs in the right directory", func() {
+			Expect(createTlsFiles(hostname, cmdRunner)).To(Succeed())
+
+			currDir, err := os.Getwd()
+
+			if err != nil {
+				Fail("Unable to check current working directory to verify we're in the certificates directory.")
+			}
+			Expect(argList[0]).To(Equal(fmt.Sprintf("mkcert %v", hostname)))
+			Expect(currDir).To(Equal(certificatesDir))
+		})
+
+		It("returns an error if the command fails", func() {
+			err = fmt.Errorf("couldn't do that chief!")
+
+			result := createTlsFiles(hostname, cmdRunner)
+			Expect(result).To(Equal(err))
+		})
+	})
+
+	Describe("addFilesToConfig", func() {
+		var (
+			testData = `
+tls:
+  certificates:
+    - certFile: /example.pem
+		  keyFile: /example-key.pem
+`
+			expectedResult map[interface{}]interface{}
+		)
+
+		BeforeEach(func() {
+
+			expectedResult = make(map[interface{}]interface{})
+
+			expectedConfig := `
+tls:
+  certificates:
+    - certFile: /example.pem
+		  keyFile: /example-key.pem
+    - certFile: /usr/src/app/certs/example.com.pem
+      keyFile: /usr/src/app/certs/example.com-key.pem
+`
+			yaml.Unmarshal([]byte(expectedConfig), &expectedResult)
+		})
+
+		It("adds the hostname files to the config", func() {
+			result, _ := addFilesToConfig(hostname, []byte(testData))
+
+			resultUnmarshalled := make(map[interface{}]interface{})
+
+			yaml.Unmarshal(result, &resultUnmarshalled)
+			Expect(resultUnmarshalled).To(Equal(expectedResult))
+		})
+
+		It("returns an error if it can't unmarshal the config", func() {
+			testData = `
+tls:
+  certificates:
+	  ---- invalid yaml -----
+`
+			Expect(addFilesToConfig(hostname, []byte(testData))).NotTo(Succeed())
+		})
+	})
+
+	Describe("createCertFileName", func() {
+		It("creates the right file name", func() {
+			Expect(createCertFileName(hostname)).To(Equal("example.com.pem"))
+		})
+	})
+
+	Describe("createKeyFileName", func() {
+		It("creates the right file name", func() {
+			Expect(createKeyFileName(hostname)).To(Equal("example.com-key.pem"))
+		})
+	})
+})
